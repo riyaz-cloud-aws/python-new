615e363965b7243d74a38eecb5ec0cafae9aa19d
riyaz_cloud_aws <riyazaws1997@gmail.com>
2025-07-18 03:42:49 +0000

    command_line_argv

---
+++ data types/command argv.py
@@ -0,0 +1,35 @@
+import sys
+
+def add():
+    add = num1 + num2
+    return add
+def sub():
+    sub = num1 - num2
+    return sub
+def mul():
+    mul = num1 * num2 
+    return mul
+def div():
+    div = num1 / num2
+    return div
+
+num1 = int(sys.argv[1])
+operation = sys.argv[2]
+num2 = int(sys.argv[3])
+
+if operation == "add":
+    output = add()
+    print(output)
+    
+if operation =="sub":
+    output= sub()
+    print(output)
+    
+if operation == "mul":
+    output = mul()
+    print(output)
+    
+if operation == "div":
+    output = div()
+    print(output)
+
